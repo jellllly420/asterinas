0cb149d4275088022f19841b84542ac498122560
Zejun Zhao <jelly.zhao.42@gmail.com>
2025-05-28 15:35:00 +0800

    Add RISC-V PLIC support

--- ostd/src/arch/loongarch/irq/mod.rs
+++ ostd/src/arch/loongarch/irq/mod.rs
@@ -13,3 +13,21 @@ pub(crate) use remapping::IrqRemapping;
 
 pub(crate) const IRQ_NUM_MIN: u8 = 0;
 pub(crate) const IRQ_NUM_MAX: u8 = 255;
+
+pub(crate) struct HwIrqLine {
+    irq_num: u8,
+}
+
+impl HwIrqLine {
+    pub(super) fn new(irq_num: u8) -> Self {
+        Self { irq_num }
+    }
+
+    pub(crate) fn irq_num(&self) -> u8 {
+        self.irq_num
+    }
+
+    pub(crate) fn ack(&self) {
+        chip::complete(self.irq_num);
+    }
+}
